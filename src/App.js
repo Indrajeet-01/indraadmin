cfba709f9521133f7c30789234baa7f787b07dc7
Indrajeet Singh <indrajeetsinghyadav4@gmail.com>
2023-04-23 10:46:46 +0530

    seventh

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
 import "./app.css"
 import Home from "./pages/home/Home";
-import { BrowserRouter as Router, Switch, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router,  Route, Routes } from "react-router-dom";
 import UserList from "./pages/userList/UserList";
+import User from "./pages/user/User";
 
 
 function App() {
@@ -18,6 +19,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/users" element={<UserList/>} />
+        <Route path="/user/:userId" element={<User/>} />
+
       </Routes>
       </div>
     </Router>
